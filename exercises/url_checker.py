2771ecc7c4093c76e5b006e442ed72b542fc436f
bentevo <42913214+bentevo@users.noreply.github.com>
2018-10-10 10:34:02 +0200

    Update url_checker.py

--- exercises/url_checker.py
+++ exercises/url_checker.py
@@ -1,23 +1,27 @@
 import requests 
 
+# enter the user's url
 user_url = input("Please enter your url here. ").strip()
 
+# defining the variables 
 req = requests.get(user_url)
 status = req.status_code
 headers = req.headers
 
+# printing the error message if the status code is not 200 (OK)
 if status is not 200:
 	print(f'Something went wrong, please try again. ')
 	exit()
 
+# printing the headers 
 for key, value in headers.items():
 	print(f'{key}: {value}')
 
 text = req.text
 
+# extra: looping the lines (splitting the text by line) until the index is 10 so it displays the first 10 lines.
 index = 0
 while index < 10:
 	line = text.splitlines()[index]
 	print(line)
 	index = index + 1
-
